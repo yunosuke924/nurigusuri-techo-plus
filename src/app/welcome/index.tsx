54c015b054ca95dfdfd966d16ed46bcda1ee6d53
yunosuke924 <y.minegishi0924@gmail.com>
2024-03-21 21:57:05 +0900

    fix: ウェルカムページのindexページ作成

--- src/app/welcome/index.tsx
+++ src/app/welcome/index.tsx
@@ -1,7 +1,8 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import { useNavigation } from 'expo-router'
 import { useEffect } from 'react'
 import LogoutButton from '@/components/LogoutButton'
+import { COLOR } from '@/styles/colors'
 
 const List = (): JSX.Element => {
   const navigation = useNavigation()
@@ -10,13 +11,31 @@ const List = (): JSX.Element => {
       headerRight: () => <LogoutButton />
     })
   }, [])
-  return <View style={Styles.constainer}></View>
+  return (
+    <View style={Styles.constainer}>
+      <Text style={Styles.title}>塗り薬手帳+の機能 その1</Text>
+    </View>
+  )
 }
 
 const Styles = StyleSheet.create({
   constainer: {
     flex: 1, // 画面いっぱいに広げる
-    backgroundColor: '#fffff'
+    backgroundColor: '#fffff',
+    alignItems: 'center'
+  },
+  title: {
+    color: COLOR.brown80,
+    fontSize: 16,
+    textAlign: 'center',
+    margin: 10,
+    padding: 10,
+    top: 0,
+    borderColor: COLOR.brown80,
+    borderWidth: 1,
+    borderRadius: 20,
+    fontWeight: 'bold',
+    width: 300
   }
 })
 
