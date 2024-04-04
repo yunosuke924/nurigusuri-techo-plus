6ea2aeae29e28a7d4eecca281cff71ea4ff23d79
yunosuke924 <y.minegishi0924@gmail.com>
2024-04-04 10:23:20 +0900

    add: reanimatedとcarouselの導入

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,9 +1,12 @@
-import { View, StyleSheet, Text, ScrollView } from 'react-native'
+import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native'
 import { useNavigation } from 'expo-router'
 import { useEffect } from 'react'
 import LogoutButton from '@/components/LogoutButton'
 import { COLOR } from '@/styles/colors'
 import { Icon } from 'react-native-elements'
+import Carousel from 'react-native-reanimated-carousel'
+
+const width = Dimensions.get('window').width
 
 const List = (): JSX.Element => {
   const navigation = useNavigation()
@@ -40,13 +43,131 @@ const List = (): JSX.Element => {
             </Text>
           </View>
         </View>
-        <View style={{ flexDirection: 'row' }}>
-          <Text
-            style={{ fontSize: 18, color: COLOR.brown80, fontWeight: 'bold' }}
+        <View style={styles.InformationContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingRight: 18
+            }}
           >
-            サマリー
-          </Text>
-          <Icon name='settings' color={COLOR.white} />
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOR.brown80,
+                fontWeight: 'bold',
+                marginTop: 8,
+                marginBottom: 8
+              }}
+            >
+              サマリー
+            </Text>
+            <Icon name='settings' color={COLOR.gray30} />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Carousel
+              loop={false}
+              width={width / 2}
+              style={{ width }}
+              height={width / 2}
+              autoPlay={false}
+              data={[...new Array(6).keys()]}
+              scrollAnimationDuration={1000}
+              onSnapToItem={(index) => {
+                console.log('current index:', index)
+                console.log('width', width)
+              }}
+              renderItem={({ index }) => {
+                switch (index) {
+                  case 0:
+                    return (
+                      <View
+                        style={[
+                          styles.summaryCard,
+                          { backgroundColor: COLOR.green40 }
+                        ]}
+                      >
+                        <Text>{index + 1}</Text>
+                      </View>
+                    )
+                  case 1:
+                    return (
+                      <View
+                        style={[
+                          styles.summaryCard,
+                          { backgroundColor: COLOR.orange40 }
+                        ]}
+                      >
+                        <Text>{index + 1}</Text>
+                      </View>
+                    )
+                }
+                return (
+                  <View
+                    style={[
+                      styles.summaryCard,
+                      { backgroundColor: COLOR.brown40 }
+                    ]}
+                  >
+                    <Text>{index + 1}</Text>
+                  </View>
+                )
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.InformationContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingRight: 18
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOR.brown80,
+                fontWeight: 'bold',
+                marginTop: 8,
+                marginBottom: 8
+              }}
+            >
+              塗り薬の情報
+            </Text>
+            <Icon name='settings' color={COLOR.gray30} />
+          </View>
+          <View style={styles.InformationCard}></View>
+          <View style={styles.InformationCard}></View>
+          <View style={styles.InformationCard}></View>
+        </View>
+        <View style={styles.InformationContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingRight: 18
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOR.brown80,
+                fontWeight: 'bold',
+                marginTop: 8,
+                marginBottom: 8
+              }}
+            >
+              塗り薬の情報
+            </Text>
+            <Icon name='settings' color={COLOR.gray30} />
+          </View>
+          <View style={styles.InformationCard}></View>
+          <View style={styles.InformationCard}></View>
+          <View style={styles.InformationCard}></View>
         </View>
       </ScrollView>
       <View style={styles.bottomNaviagation}></View>
@@ -75,6 +196,28 @@ const styles = StyleSheet.create({
     width: '100%',
     bottom: 0,
     borderRadius: 64
+  },
+  InformationContainer: {
+    marginRight: 18,
+    marginLeft: 18
+  },
+  InformationCard: {
+    backgroundColor: COLOR.white,
+    height: 96,
+    borderRadius: 40,
+    marginTop: 8,
+    marginBottom: 8
+  },
+  sliderContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  summaryCard: {
+    backgroundColor: COLOR.white,
+    height: 196,
+    width: 163,
+    borderRadius: 40,
+    margin: 8
   }
 })
 
